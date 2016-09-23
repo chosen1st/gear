632e8c8a77d69e3832dcc4365f27e1deeb1e5084
Lin Shengtao <12change@tongji.edu.cn>
2016-09-24 00:09:55 +0800

    Update downloadallppt_xuetangx.cpp

--- xuetangx/downloadallppt_xuetangx.cpp
+++ xuetangx/downloadallppt_xuetangx.cpp
@@ -4,10 +4,17 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	int count[16]={0,7,7,4,8,8,6,10,12,14,14,19,15,12,14,5};
-	for (int i = 1; i < 16; ++i)
+	for (int i = 1; i < 8; ++i)
 	{
 		while(count[i]!=0){
 			cout<<"http://www.xuetangx.com/c4x/TsinghuaX/20740084X/asset/LinuxCPP"<<setw(2)<<setfill('0')<<setiosflags(ios::right)<<i<<setw(2)<<setfill('0')<<setiosflags(ios::right)<<count[i]<<".pptx"<<endl;
+			count[i]--;
+		}
+	}
+	for (int i = 8; i < 16; ++i)
+	{
+		while(count[i]!=0){
+			cout<<"http://www.xuetangx.com/c4x/TsinghuaX/20740084X/asset/LinuxCpp__"<<setw(2)<<setfill('0')<<setiosflags(ios::right)<<i<<setw(2)<<setfill('0')<<setiosflags(ios::right)<<count[i]<<".pptx"<<endl;
 			count[i]--;
 		}
 	}
